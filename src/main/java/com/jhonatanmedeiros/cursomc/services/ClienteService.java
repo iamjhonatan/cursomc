d7cc3b289367066ac5ebd1eaf8a88a114f0d8d62
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-01-28 10:22:56 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/jhonatanmedeiros/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.jhonatanmedeiros.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jhonatanmedeiros.cursomc.domain.Cliente;
+import com.jhonatanmedeiros.cursomc.repositories.ClienteRepository;
+import com.jhonatanmedeiros.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional <Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
