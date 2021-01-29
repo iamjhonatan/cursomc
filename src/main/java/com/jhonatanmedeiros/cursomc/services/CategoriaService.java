177fca34fba9bbc92467a03ae190b6b5b42e9096
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-01-29 17:20:22 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/jhonatanmedeiros/cursomc/services/CategoriaService.java
+++ src/main/java/com/jhonatanmedeiros/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jhonatanmedeiros.cursomc.domain.Categoria;
+import com.jhonatanmedeiros.cursomc.dto.CategoriaDTO;
 import com.jhonatanmedeiros.cursomc.repositories.CategoriaRepository;
 import com.jhonatanmedeiros.cursomc.services.exceptions.DataIntegrityException;
 import com.jhonatanmedeiros.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
