5c049994c381a29194233a113f23999e967d001b
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-02-02 15:48:45 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/com/jhonatanmedeiros/cursomc/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.jhonatanmedeiros.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private Environment env;
+	
+	//Endpoints liberados por padrão
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**",
+	};
+	
+	// caminho onde é permitido apenas recuperar os dados (listar)
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**"
+	};
+	
+	//permitindo todas as requisições do vetor 'PUBLIC_MATCHERS' | os demais, exija autenticação
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		// se nos profiles ativos, eu estiver no profile 'test', significa que eu vou querer acessar o H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		http.cors().and().csrf().disable();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	// dando um acesso básico de múltiplas fontes para todos os caminhos | permitindo o acesso aos endpoints
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+}
