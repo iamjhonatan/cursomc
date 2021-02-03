25364b111c3424fb29b4dcd79ab4e51f891d17c0
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-02-03 17:20:06 -0300

    Implementando autorização

---
+++ src/main/java/com/jhonatanmedeiros/cursomc/security/JWTAuthorizationFilter.java
@@ -0,0 +1,59 @@
+package com.jhonatanmedeiros.cursomc.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+// analisar o token e verificar se é válido
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter{
+	
+	private JWTUtil jwtUtil;
+	
+	private UserDetailsService userDetailsService;
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil, UserDetailsService userDetailsService) {
+		super(authenticationManager);
+		this.jwtUtil = jwtUtil;
+		this.userDetailsService = userDetailsService;
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request,
+									HttpServletResponse response,
+									FilterChain chain) throws IOException, ServletException {
+		// pegando o token no header da requisição | campo 'Authorization'
+		String header = request.getHeader("Authorization");
+		
+		if (header != null && header.startsWith("Bearer ")) {
+			UsernamePasswordAuthenticationToken auth = getAuthentication(header.substring(7));
+			// procedimento para liberar o acesso do usuário que está tentando o acesso ao endpoint
+			if (auth != null) { // != nulo, token ok
+				// liberando o acesso no filtro
+				SecurityContextHolder.getContext().setAuthentication(auth);
+			}
+		}
+		// continuando a requisição normalmente
+		chain.doFilter(request, response);
+		
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(String token) {
+		if (jwtUtil.tokenValido(token)) {
+			String username = jwtUtil.getUsername(token);
+			UserDetails user = userDetailsService.loadUserByUsername(username); // buscando o usuário no database
+			return new UsernamePasswordAuthenticationToken(username, null, user.getAuthorities());
+		}
+		return null;
+	}
+
+}
--- src/main/java/com/jhonatanmedeiros/cursomc/security/JWTUtil.java
+++ src/main/java/com/jhonatanmedeiros/cursomc/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -24,6 +25,39 @@ public class JWTUtil {
 				.setExpiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
+	}
+	
+	// Claims é um tipo do JWT que armazena as reivindicações do token
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			String username = claims.getSubject(); // função para retornar o usuário
+			Date expirationDate = claims.getExpiration(); // pegando a data de expiração do token
+			Date now = new Date (System.currentTimeMillis()); // pegando a data atual para testar se o token está expirado
+			if (username !=  null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+
+	private Claims getClaims(String token) {
+		
+		// função que recupera os claims a partir de um token
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		}
+		catch (Exception e) {
+			return null;
+		}
 	}
 	
 }
