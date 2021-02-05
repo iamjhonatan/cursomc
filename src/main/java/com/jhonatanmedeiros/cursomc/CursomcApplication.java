8d5eb916ecd9b0b97420ce54e222e28532524137
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-02-05 14:56:50 -0300

    Enviando imagem via endpoint

--- src/main/java/com/jhonatanmedeiros/cursomc/CursomcApplication.java
+++ src/main/java/com/jhonatanmedeiros/cursomc/CursomcApplication.java
@@ -1,17 +1,13 @@
 package com.jhonatanmedeiros.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.jhonatanmedeiros.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
-	@Autowired
-	private S3Service s3Service;
+	
 	
 	
 	public static void main(String[] args) {
@@ -20,7 +16,7 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\jhona\\OneDrive\\Imagens\\Wallpapers\\lbjj.jpg");
+		
 		
 		
 		
